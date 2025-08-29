3355d88dd607c1a3e09f21e01de18f1a6371498c
Juan Israel Garcia Celedon <Israel.Garcia@perficient.com>
2025-08-29 18:52:36 +0000

    Deduplications and tests added

--- models/staging/stg_epidemiology.sql
+++ models/staging/stg_epidemiology.sql
@@ -1,12 +1,26 @@
-select
-    log_date,
-    location_key,
-    new_confirmed,
-    new_deceased,
-    new_recovered,
-    new_tested,
-    cumulative_confirmed,
-    cumulative_deceased,
-    cumulative_recovered,
-    cumulative_tested
-from {{ source('public_schema', 'raw_epidemiology_csv') }}
+with source_epidemiology as (
+    select
+        date as log_date,
+        location_key,
+        new_confirmed,
+        new_deceased,
+        new_recovered,
+        new_tested,
+        cumulative_confirmed,
+        cumulative_deceased,
+        cumulative_recovered,
+        cumulative_tested,
+        src_filename,
+        src_file_last_modified
+    from {{ source('public_schema', 'raw_epidemiology_csv') }}
+),
+deduplicated as (
+    select *
+    from source_epidemiology
+    qualify row_number() over(
+        partition by log_date, location_key
+        order by src_file_last_modified desc, src_filename desc
+    ) = 1
+)
+
+select * from deduplicated
